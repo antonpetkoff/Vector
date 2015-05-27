3967246bb6b885c7fd01c5afd30622917c8c00f1
tonynho <tonynho010@gmail.com>
2015-05-27 10:07:35 +0300

    vector big three

--- include/Vector.h
+++ include/Vector.h
@@ -1,21 +1,58 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <memory>
+
 template<typename T, typename A = std::allocator<T> >
 class Vector
 {
 private:
     VectorBase<T> base;
+    void destroy_elements();
 public:
-
+    explicit Vector(size_t n, const T& val = T(), const A& alloc = A());
+    Vector(const Vector& other);
+    ~Vector();
+    int capacity() const    { return base.last - base.first; }
+    int size() const        { return base.space - base.first; }
+    T* begin() const        { return base.first; }
+    T* end() const          { return base.space; }
 };
 
-
 template<typename T, typename A>
-void Vector<T, A>::printState()
+Vector<T, A>::Vector(size_t count, const T& val, const A& alloc)
+    : base(alloc, count)
 {
-    std::cout << "state\n";
+    // uses the copy constructor of T
+    std::cout << "Vector constructor called\n";
+    std::uninitialized_fill(base.first, base.first + count, val);
 }
 
+template<typename T, typename A>
+Vector<T, A>::Vector(const Vector& other)
+    : base(other.base.alloc, other.size())
+{
+    std::cout << "Vector copy constructor called\n";
+    std::uninitialized_copy(other.begin(), other.base.space, base.first);
+}
+
+template<typename T, typename A>
+void Vector<T, A>::destroy_elements()
+{
+    std::cout << "Vector destructor called\n";
+    for (T* cursor = base.first; cursor != base.space; ++cursor)
+    {
+        cursor->~T();
+    }
+    base.space = base.first;
+    std::cout << "Vector destructor finished\n";
+    base.printState();
+}
+
+template<typename T, typename A>
+Vector<T, A>::~Vector()
+{
+    destroy_elements();
+}
 
 #endif // VECTORBASE_H
--- include/VectorBase.h
+++ include/VectorBase.h
@@ -4,30 +4,53 @@
 template<typename T, typename A = std::allocator<T> >
 struct VectorBase
 {
-    A alloc_;
-	T* first_;      // start of allocated space, start of element sequence
-	T* space_;      // end of element sequence, start of space allocated for possible expansion
-	T* last_;       // end of allocated space
+    A alloc;
+	T* first;      // start of allocated space, start of element sequence
+	T* space;      // end of element sequence, start of space allocated for possible expansion
+	T* last;       // end of allocated space
 
     void printState();
 
-    VectorBase(const A& alloc, typename A::size_type n)
-        : alloc_(alloc), first_(alloc_.allocate(n)), space_(first_ + n), last_(first_ + n) {printState();}
+    VectorBase(const A& a, typename A::size_type n)
+        : alloc(a), first(alloc.allocate(n)), space(first + n), last(first + n) {printState();}
 
     VectorBase(const VectorBase& other) = delete;
     VectorBase& operator=(const VectorBase& other) = delete;
 
-    VectorBase(const VectorBase&& other);
-    VectorBase& operator=(const VectorBase&& other);
+    VectorBase(VectorBase&& other);
+    VectorBase& operator=(VectorBase&& other);
 
-    ~VectorBase() {alloc_.deallocate(first_, last_ - first_);}
+    ~VectorBase();
 };
+
+template<typename T, typename A>
+VectorBase<T, A>::~VectorBase()
+{
+    std::cout << "VectorBase destructor called\n";
+    alloc.deallocate(first, last - first);
+    std::cout << "VectorBase destructor finished\n";
+}
 
 template<typename T, typename A>
 void VectorBase<T, A>::printState()
 {
-    std::cout << "vector base size: " << last_ - first_ << "\n";
+    std::cout << "vector base size: " << last - first << "\n";
 }
 
+template<typename T, typename A>
+VectorBase<T, A>::VectorBase(VectorBase&& other)
+    : alloc(other.alloc), first(other.first), space(other.space), last(other.last)
+{
+    std::cout << "VectorBase move constructor\n";
+    other.first = other.space = other.last = nullptr;
+}
+
+template<typename T, typename A>
+VectorBase<T, A>& VectorBase<T, A>::operator=(VectorBase&& other)
+{
+    std::cout << "VectorBase move assignment\n";
+    std::swap(*this, other);
+    return *this;
+}
 
 #endif // VECTORBASE_H
